1fc4a01e3d4f82459939dbb1292d767682b6bfdb
nguyenthanhoang <nthoang0708@gmail.com>
2025-04-13 10:00:16 +0700

    Fix lỗi login

--- js/login.js
+++ js/login.js
@@ -2,17 +2,18 @@ function login(event) {
     event.preventDefault();
     let username = document.getElementById('login_user').value.trim();
     let password = document.getElementById('login_password').value.trim();
-    let Login_Message = document.getElementById('Login_Message')
+    let Login_Message = document.getElementById('Login_Message');
 
     let users = localStorage.getItem('users') ? JSON.parse(localStorage.getItem('users')) : {};
-    let storedUser =users[username];
-    console.log(storedUser)
-    console.log(username)
-    if (storedUser && storedUSer.password === password) {
+    console.log(users);
+
+    let storedUser = users[username];
+
+    if (storedUser && storedUser.password === password) {
+        alert("Đăng nhập thành công!!!")
         window.location.href = 'index.html';
     } else {
-        Login_Message.innerText = "Tài khoản hoặc mật khẩu đã tồn tại"
+        Login_Message.innerText = "Tài khoản hoặc mật khẩu đã tồn tại";
         Login_Message.style.color = 'red';
     }
-
 }
